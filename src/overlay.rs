3f6a87e075510944585d13980b40ffb687b5ba34
AlphaWolf69 <AlphaWolf69@gmail.com>
2026-06-13 14:27:18 +0300

    not transparent, not click throgh, but latest ImGui version

--- src/overlay.rs
+++ src/overlay.rs
@@ -15,14 +15,12 @@ impl OverlayApp {
     pub fn run(self) {
         let game_process = self.game_process;
 
-        let window_size = egui::vec2(280.0, 20.0);
-
         let viewport = egui::ViewportBuilder::default()
             .with_title("Text Overlay")
-            .with_inner_size(window_size)
+            .with_inner_size([280.0, 20.0])
             .with_decorations(false)
             .with_transparent(true)
-            .with_always_on_top()
+            .with_window_level(egui::WindowLevel::AlwaysOnTop)
             .with_resizable(false);
 
         let native_options = NativeOptions {
@@ -69,11 +67,7 @@ impl TextApp {
 }
 
 impl eframe::App for TextApp {
-    fn update(
-        &mut self,
-        ctx: &egui::Context,
-        _frame: &mut eframe::Frame,
-    ) {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
         let mut visuals = egui::Visuals::dark();
 
         visuals.override_text_color = Some(egui::Color32::WHITE);
@@ -82,14 +76,12 @@ impl eframe::App for TextApp {
         visuals.faint_bg_color = egui::Color32::TRANSPARENT;
         visuals.extreme_bg_color = egui::Color32::TRANSPARENT;
 
-        ctx.set_visuals(visuals);
+        ui.ctx().set_visuals(visuals);
 
         egui::CentralPanel::default()
             .frame(egui::Frame::NONE)
-            .show(ctx, |ui| {
+            .show_inside(ui, |ui| {
                 ui.horizontal_centered(|ui| {
-                    let font_size = 20.0;
-
                     let addr = self.game_process.base_address + 0x3335638;
 
                     let label = match self.game_process.read_string(addr, 256) {
@@ -99,7 +91,7 @@ impl eframe::App for TextApp {
 
                     ui.label(
                         egui::RichText::new(label)
-                            .size(font_size)
+                            .size(20.0)
                             .color(egui::Color32::WHITE),
                     );
                 });
@@ -107,6 +99,6 @@ impl eframe::App for TextApp {
                 ui.add_space(6.0);
             });
 
-        ctx.request_repaint();
+        ui.ctx().request_repaint();
     }
 }
